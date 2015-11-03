7a352764311f4c53b35a713b8f1121b12d58b019
Germain Boue <boue.germain@gmail.com>
2015-11-03 08:11:25 -0800

    Create node object

--- linked_list.js
+++ linked_list.js
@@ -1,3 +1,11 @@
 function LinkedList(){
   this.head = null;
 }
+
+LinkedList.prototype.push = function(val){
+  //node has a value and a pointer to null
+  var node = {
+    value: val,
+    next: null
+  }
+}
