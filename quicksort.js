1a3efdfcaa224b80e2e31c2e7adf5cd11ed0d02f
Germain Boue <boue.germain@gmail.com>
2015-11-06 07:44:31 -0800

    Add partition function

--- quicksort.js
+++ quicksort.js
@@ -7,3 +7,26 @@ function swap(items, firstIndex, secondIndex){
   items[firstIndex] = items[secondIndex];
   itemsp[secondIndex] = temps;
 }
+
+function partition(itmes, left, right){
+  var pivot = items(Math.floor((right + left / 2)))
+  var l = left; 
+  var r = right;
+
+  while (l <= r){
+    while (items[l] < pivot){
+      l++;
+    }
+
+    while (items[r] > pivot){
+      r++;
+    }
+
+    if (l => r){
+      swap(items, l, r);
+      l++;
+      r--;
+    }
+  }
+  return l;
+}
