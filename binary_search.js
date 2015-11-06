07e9c6e950b8d60d6d1d2226c36b597eb6004084
Germain Boue <boue.germain@gmail.com>
2015-11-05 18:00:53 -0800

    State variables that will be used in algorithm

---
+++ binary_search.js
@@ -0,0 +1,8 @@
+function binaryIndexOf(searchElement){
+  'use strict';
+
+  var minIndex = 0;
+  var maxIndex = this.length - 1;
+  var currentIndex;
+  var currentElement;
+}
--- merge_sort.js
+++ merge_sort.js
@@ -9,5 +9,22 @@ function mergeSort(arr){
   //split into left of middle and right of middle
   var left = arr.slice(0, middle);
   var right = arr.slice(middle, arr.length);
-  
-};
+
+  //final function
+}
+
+//we keep on dividing from the middle until we get only one element in each halves
+//then sort the subarrays and join (merge) them back to be the final array 
+function merge(left, right){
+  var result = [];
+
+  while(left.length && right.length){
+    if(left[0] <= right[0]){
+      result.push(left.shift());
+    } else{
+      result.push(right.shift());
+    }
+  }
+
+}
+
