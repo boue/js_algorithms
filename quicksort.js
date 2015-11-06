2880a75ae2e409d5bedc51b6aec61a05f01e46ad
Germain Boue <boue.germain@gmail.com>
2015-11-06 07:54:19 -0800

    Quicksort function implemented

--- quicksort.js
+++ quicksort.js
@@ -1,6 +1,7 @@
 //partition function
 //swap
 //avoid worst case scenario by picking pivot as first or last item in case array is sorted
+//ask is the array sorted? To optimize best if not and 0+ values in array
 
 function swap(items, firstIndex, secondIndex){
   var temp = items[firstIndex];
@@ -8,7 +9,7 @@ function swap(items, firstIndex, secondIndex){
   itemsp[secondIndex] = temps;
 }
 
-function partition(itmes, left, right){
+function partition(items, left, right){
   var pivot = items(Math.floor((right + left / 2)))
   var l = left; 
   var r = right;
@@ -28,5 +29,26 @@ function partition(itmes, left, right){
       r--;
     }
   }
+  //return left pointer to know where to start partitioning the next time
   return l;
 }
+
+function quicksort(items, left, right){
+  var index;
+
+  //If there are two or more items in the array then it is partitioned.
+  if (items.length > 1){
+    index = partition(items, left, right);
+
+    if (left < index - 1){
+      quicksort(items, left, index - 1);
+    }
+
+    if (index < right){
+      quicksort(items, index, right);
+    }
+  }
+  return items;
+}
+
+var result = quicksort(items, 0, items.length - 1);
