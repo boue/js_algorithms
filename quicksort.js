01b09b8683fbe49f636b963d8914ed26f92aa513
Germain Boue <boue.germain@gmail.com>
2015-11-06 07:34:43 -0800

    Implement Swap function

---
+++ quicksort.js
@@ -0,0 +1,9 @@
+//partition function
+//swap
+//avoid worst case scenario by picking pivot as first or last item in case array is sorted
+
+function swap(items, firstIndex, secondIndex){
+  var temp = items[firstIndex];
+  items[firstIndex] = items[secondIndex];
+  itemsp[secondIndex] = temps;
+}
