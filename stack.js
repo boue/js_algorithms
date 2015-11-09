6444065f6babcd76074d68881df11ff443e6375c
Germain Boue <boue.germain@gmail.com>
2015-11-08 20:01:38 -0800

    Edge case for when pop does not have any more values in storage

--- stack.js
+++ stack.js
@@ -8,3 +8,21 @@ Stack.prototype.push = function(data){
   var size = this._size++;
   var._storage[size] = data;
 }
+
+//pop must remove the most recent added data
+Stack.prototype.pop = function(){
+  var size = this._size,
+      deletedData;
+
+  //most recent since size represents index of latest 
+  //size to make sure we can't pop and changes size to -1 once we reach empty object
+  if(size){
+    deletedData = this._storage[size];
+
+  //use the key as reference to delete data
+    delete this._storage[size];
+    this.size--;
+
+    return deletedData;
+  };
+};
