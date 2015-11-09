028cc47eedec49b3a7f6ab10eec7fbba80045a87
Germain Boue <boue.germain@gmail.com>
2015-11-08 18:44:23 -0800

    Implement push method

--- stack.js
+++ stack.js
@@ -2,3 +2,9 @@ function Stack(){
   this._size = 0;
   this._storage = 0;
 }
+
+//declare size by adding one and using that var as a key with data as the value
+Stack.prototype.push = function(data){
+  var size = this._size++;
+  var._storage[size] = data;
+}
