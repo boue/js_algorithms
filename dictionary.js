50df4f233da5ec0a73c9dba3c733e6e7c60122ad
Germain Boue <boue.germain@gmail.com>
2015-11-09 17:55:07 -0800

    All functions work

--- dictionary.js
+++ dictionary.js
@@ -1,22 +1,22 @@
 function Dictionary(){
-  this.datastore = new Array();
+  this.datastore = [];
 }
 
 Dictionary.prototype.add = function(key, value){
   this.datastore[key] = value;
-}
+};
 
 Dictionary.prototype.find = function(key){
   return this.datastore[key];
-}
+};
 
 //delete takes a key as an argument and deleted both key + val
 Dictionary.prototype.remove = function(key){
   delete this.datastore[key];
-}
+};
 
-Dictionary.prototype.showAll = function(){
-  for each(var key in Object.keys(this.datastore)){
-    print(key + "=>" this.datastore[key]);
-  }
-}
+var pbook = new Dictionary(); 
+pbook.add("Mike","123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+console.log("David's extension: " + pbook.find("David")); pbook.remove("David");
