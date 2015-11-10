b5231ce0d849ff4e087dce750aaa2579086d7356
Germain Boue <boue.germain@gmail.com>
2015-11-09 17:43:11 -0800

    All functions for the dictionary data structure using prototype

--- dictionary.js
+++ dictionary.js
@@ -10,3 +10,13 @@ Dictionary.prototype.find = function(key){
   return this.datastore[key];
 }
 
+//delete takes a key as an argument and deleted both key + val
+Dictionary.prototype.remove = function(key){
+  delete this.datastore[key];
+}
+
+Dictionary.prototype.showAll = function(){
+  for each(var key in Object.keys(this.datastore)){
+    print(key + "=>" this.datastore[key]);
+  }
+}
