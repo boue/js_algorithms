77d3aaf8b1d13d27dcd3493d8fa33275a0bd6195
Germain Boue <boue.germain@gmail.com>
2015-11-12 20:36:22 -0800

    Add BST class and prototype to insert data within BT

--- binary_tree_2.js
+++ binary_tree_2.js
@@ -4,6 +4,7 @@
 //depth of a tree is number of layers in a tree
 //binary search tree has lesser value on left node with right node having bigger value
 
+//class for Node
 function Node(data, left, right){
   this.data = data;
   //left and right are links to other nodes
@@ -13,4 +14,35 @@ function Node(data, left, right){
 
 Node.prototype.show = function(){
   this.data;
+}
+
+//class for Binary Search Tree
+function BST(){
+  this.root = null;
+}
+
+BST.prototype.insert(data) = function(){
+  var n = new Node(data, null, null);
+  if (this.root == null) {
+    this.root = n;
+  } else {
+    var current = this.root;
+    var parent;
+    while (true){
+      parent = current;
+      if (data < current.data) {
+        current = current.left;
+        if (current == null){
+          parent.left = n;
+          break;
+        }
+      } else {
+        current = current.right;
+        if (current == null) {
+          parent.right = n;
+          break;
+        } 
+      }
+    }
+  }
 }
