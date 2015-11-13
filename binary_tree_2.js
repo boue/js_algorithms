1e2c674fcb9dd528a953bf1adfe9ded968deb2eb
Germain Boue <boue.germain@gmail.com>
2015-11-12 21:27:10 -0800

    Substantial changes, console logging nums gets us the full tree, traversing changes need to be adjusted

--- binary_tree_2.js
+++ binary_tree_2.js
@@ -21,7 +21,7 @@ function BST(){
   this.root = null;
 }
 
-BST.prototype.insert(data) = function(){
+BST.prototype.insert = function(data){
   var n = new Node(data, null, null);
   if (this.root == null) {
     this.root = n;
@@ -46,3 +46,36 @@ BST.prototype.insert(data) = function(){
     }
   }
 }
+
+//LNR
+BST.prototype.inOrder = function(node){
+  if (!(node == null)){
+    //left
+    this.inOrder(node.left);
+    //node
+    console.log(node.show() + " ");
+    //right
+    this.inOrder(node.right);
+  }
+}
+
+//pre - NLR
+//post order is LRN
+BST.prototype.postOrder = function(node){
+  if (!(node == null)){
+    this.postOrder(node.left);
+    this.postOrder(node.right);
+    console.log(node.show() = " ");
+  }
+}
+
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+console.log("Inorder traversal : ");
+nums.inOrder(nums.root);
