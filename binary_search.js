868363b8bb2c9a02551f1c41ae37b40ab1fd2826
Germain Boue <boue.germain@gmail.com>
2015-11-14 14:53:54 -0800

    Now have a count function to check on how many times the searched item is present in arr

--- binary_search.js
+++ binary_search.js
@@ -43,6 +43,23 @@ function binSearch(arr, data){
 
 function count(arr, data){
   var count = 0;
-  var position
-
+  var position = binSearch(arr, data);
+  if (position > -1){
+    ++count;
+    for (var i = position-1; i > 0; --i){
+      if (arr[i] == data){
+        ++count;
+      } else {
+        break;
+      }
+    }
+    for (var i = position+1; i < arr.length; i++){
+      if (arr[i] == data){
+        ++count;
+      } else {
+        break;
+      }
+    }
+  } 
+  return count;
 }
