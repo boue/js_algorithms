0680aeeee396bbbdf50a94145b87a977bbbaf7fc
Germain Boue <boue.germain@gmail.com>
2015-11-18 21:44:27 -0800

    Finish doubly linked list

--- doubly_ll.js
+++ doubly_ll.js
@@ -1,5 +1,26 @@
-var node = {
+function Node() = {
   value: val;
   next: null,
   previous: null
+}
+
+function DoublyLinkedList(){
+  this.head = null;
+}
+
+DoublyLinkedList.prototype.push = function(val){
+  var head = this.head;
+  var current = head;
+  var previous = head;
+
+  if(!head){
+    this.head = {value: val, previous: null, next: null};
+  }
+  else{
+    while(current && current.next){
+      previous = current;
+      current = current.next;
+    }
+    current.next = {value: val, previous: current, next: null}
+  }
 }
