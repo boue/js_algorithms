ffcfeea377b30b5889d85cdf01df51bcd2f1cb81
Germain Boue <boue.germain@gmail.com>
2015-11-22 09:48:01 -0800

    Display method to show the data

--- seq_search.js
+++ seq_search.js
@@ -7,3 +7,15 @@ var seqSearch(arr, data){
     }
   }
 }
+
+function dispArr(arr){
+  for (var i = 0; i < arr.length; i++){
+    console.log(arr[i] + " ");
+    if (i % 10 == 9){
+      console.log("\n");
+    }
+  }
+  if (i % 10 != 0){
+    console.log("\n");
+  }
+}
