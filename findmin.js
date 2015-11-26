3e06e8703fceb457c1a020f6bc2bef9642384f23
Germain Boue <boue.germain@gmail.com>
2015-11-26 11:32:49 -0800

    Add a swap function

--- findmin.js
+++ findmin.js
@@ -7,3 +7,20 @@ function findMin(arr){
   }
   return min;
 }
+
+function findMax(arr){
+  var max = arr[0];
+  for (var i = 0; i < arr.length; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+//self-organization
+function swap(arr, index, index1){
+  temp = arr[index];
+  arr[index] = arr[index1];
+  arr[index1] = temp;
+}
