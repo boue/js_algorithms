6dd52adccedb57b9af79c0f3590f3b5fc69fa3a8
Germain Boue <boue.germain@gmail.com>
2015-12-16 17:01:27 -0800

    For loop that adds to the sum to know number of times

--- missing_number.js
+++ missing_number.js
@@ -2,4 +2,9 @@ function missingNumber(arr){
   var n = arr.length+1,
   sum = 0,
   expectedSum = n * (n+1/2);
+
+  for (var i = 0, len = arr.length; i < length; i++) {
+    sum += arr[i];
+  }
+  
 }
