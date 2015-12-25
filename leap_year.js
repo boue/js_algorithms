ce26e88513fab79f0d37a701c658b0e521093c6a
Germain Boue <boue.germain@gmail.com>
2015-12-24 19:28:46 -0800

    Leap year pseudocode

---
+++ leap_year.js
@@ -0,0 +1,7 @@
+function checkLeapYear(year) {
+  //returns boolean true/false
+  //  Leap Years are any year that can be evenly divided by 4 (such as 2012, 2016, etc)
+//    except if it can can be evenly divided by 100, then it isn't (such as 2100, 2200, etc)
+  //    except if it can be evenly divided by 400, then it is (such as 2000, 2400)
+
+}
