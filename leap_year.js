a7d7050064e6a833dc6e749a8e6af8ee861f1bb7
Germain Boue <boue.germain@gmail.com>
2015-12-26 15:21:56 -0800

    If/else clause

--- leap_year.js
+++ leap_year.js
@@ -1,4 +1,8 @@
 function checkLeapYear(year) {
+  if() {
+
+    return false;
+  } return true;
   //returns boolean true/false
   //  Leap Years are any year that can be evenly divided by 4 (such as 2012, 2016, etc)
 //    except if it can can be evenly divided by 100, then it isn't (such as 2100, 2200, etc)
